d75ab8009925702ffd24b6c95b139f102d93bab2
krisekchytrusek <kmajda@outlook.com>
2020-11-27 13:40:13 +0100

    Zmienne dyskretne działajo

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics import euclidean_distances
+import warnings
+warnings.filterwarnings("ignore")
 
 def importData():
     file = open("wine.data", "r")
@@ -23,9 +25,13 @@ def discretize(n,X):
     discData = est.transform(X)
     return discData
 
-def accuracy(np,lz):
-    return (np/lz)*100
+def accuracy(predictresult, ytest):
+    lz=0
+    for i in range(len(ytest)):
+        if ytest[i]==predictresult[i]:
+            lz+=1
 
+    return (lz/(len(ytest)))*100
 
 class naiveBayesClassifier(BaseEstimator,ClassifierMixin):
 
@@ -42,6 +48,7 @@ class naiveBayesClassifier(BaseEstimator,ClassifierMixin):
         arr=counts[0]
         arr=arr.astype(np.int32)
         frequency=counts[1]
+
         for i in range(len(frequency)):
             self.apriori.append(frequency[i]/len(y))
 
@@ -50,7 +57,7 @@ class naiveBayesClassifier(BaseEstimator,ClassifierMixin):
                 for k in countsX:
                     counter=0
                     for n in range(len(X)):
-                        if (int)(y[l])==m and (int)(X[n,l])==k:
+                        if (int)(y[n])==m and (int)(X[n,l])==k:
                             counter+=1
                     if self.laplace==0:
                         condProb=counter/frequency[m-1]
@@ -61,16 +68,37 @@ class naiveBayesClassifier(BaseEstimator,ClassifierMixin):
     def predict(self,X):
         countsX = np.unique(X, return_counts=True)[0]
         countsX = countsX.astype(np.int32)
+        result=[]
         for i in range(len(X)):
+            helpList=[]
             for j in range(len(self.apriori)):
                 counter=1
                 for k in range(X.shape[1]):
                     index=j*13*3+k*3+(int)(X[i][k])
                     counter=counter*self.probabilities[index]
                 counter=counter*self.apriori[j]
+                helpList.append(counter)
+            result.append(np.argmax(helpList)+1)
+        return result
 
-    def predictproba(selfsel,X):
-        print("")
+    def predictProba(self,X):
+        countsX = np.unique(X, return_counts=True)[0]
+        countsX = countsX.astype(np.int32)
+        result = []
+        for i in range(len(X)):
+            helpList = []
+            for j in range(len(self.apriori)):
+                counter = 1
+                for k in range(X.shape[1]):
+                    index = j * 13 * 3 + k * 3 + (int)(X[i][k])
+                    counter = counter * self.probabilities[index]
+                counter = counter * self.apriori[j]
+                helpList.append(counter)
+            sumList=np.sum(helpList)
+            for p in range(len(helpList)):
+                helpList[p]=helpList[p]/sumList
+            result.append(helpList)
+        return result
 
 if __name__ == '__main__':
     data=importData()
@@ -78,8 +106,19 @@ if __name__ == '__main__':
     y=data[1]
     X=discretize(3,X)
     X_train, X_test, y_train, y_test = train_test_split(X,y)
-    bayes=naiveBayesClassifier(1)
+    bayes=naiveBayesClassifier(0)
     bayes.fit(X_train,y_train)
-    bayes.predict(X_test)
+    predictResult=bayes.predict(X_test)
+    predictProbaResult=bayes.predictProba(X_test)
+    acc=accuracy(y_test, predictResult)
+    print(acc)
+    bayes1 = naiveBayesClassifier(1)
+    bayes1.fit(X_train, y_train)
+    predictResult = bayes1.predict(X_test)
+    predictProbaResult = bayes1.predictProba(X_test)
+    acc = accuracy(y_test, predictResult)
+    print(acc)
+
+
 
 
